5920c222f84d530adfc1bc88ce2b127bd7fceed7
ANNEALSYS <ANNEALSYS@DESKTOP-4U64OPC>
2026-04-24 14:17:40 +0200

    Historical directory

--- HistoricalWindow.vb
+++ HistoricalWindow.vb
@@ -52,12 +52,28 @@ Public Class HistoricalWindow
         SetChartModeCursor()
     End Sub
 
+    'Private Shared Function AppFolder(subFolder As String) As String
+    '    Dim exeDir As String = IO.Path.GetDirectoryName(
+    '        System.Reflection.Assembly.GetExecutingAssembly().Location)
+    '    Dim projDir As String = IO.Path.GetDirectoryName(IO.Path.GetDirectoryName(exeDir))
+    '    Dim dir As String = IO.Path.Combine(projDir, subFolder)
+    '    If Not IO.Directory.Exists(dir) Then IO.Directory.CreateDirectory(dir)
+    '    Return dir
+    'End Function
+
     Private Shared Function AppFolder(subFolder As String) As String
-        Dim exeDir As String = IO.Path.GetDirectoryName(
-            System.Reflection.Assembly.GetExecutingAssembly().Location)
-        Dim projDir As String = IO.Path.GetDirectoryName(IO.Path.GetDirectoryName(exeDir))
-        Dim dir As String = IO.Path.Combine(projDir, subFolder)
-        If Not IO.Directory.Exists(dir) Then IO.Directory.CreateDirectory(dir)
+        Dim baseDir As String =
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Electric_Meter_53U"
+        )
+
+        Dim dir As String = Path.Combine(baseDir, subFolder)
+
+        If Not Directory.Exists(dir) Then
+            Directory.CreateDirectory(dir)
+        End If
+
         Return dir
     End Function
 
